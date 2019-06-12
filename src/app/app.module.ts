6848316be7251dd6fb8c9ea5ea0045f8d8335a37
calebgray4570 <calebgray4570@gmail.com>
2019-06-11 20:04:28 -0600

    push test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppWrapperComponent } from 'src/app-wrapper/app-wapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppWrapperComponent
   ],
   imports: [
     BrowserModule,
